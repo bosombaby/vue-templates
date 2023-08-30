ac2b731afa7b4fc221145246166e53ab0e7f3491
bosom <1245924849@qq.com>
2023-08-30 19:52:48 +0800

    first commit

---
+++ vite-vue3-ts-pinia/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    parser: 'vue-eslint-parser',
+   env:{
+      node:true
+   },
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended'
+    ],
+
+    rules: {
+        "prettier/prettier": "warn",
+    }
+};
+
